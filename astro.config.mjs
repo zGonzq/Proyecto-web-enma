67f739a01f27ccfb9c341c70960c97716a29157a
zGonzq <gonzaloandres2508@gmail.com>
2024-01-20 13:31:06 -0300

    Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   output: 'server',
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
+  adapter: vercel(),
 });
