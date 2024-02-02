fa6e08ab251ac1ddbe1ffc0ff42f6f083efe8204
zGonzq <gonzaloandres2508@gmail.com>
2024-02-02 14:29:24 -0300

    Titulo cambiante degraduado

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,15 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
 import tailwind from "@astrojs/tailwind";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), react()],
   output: 'server',
   adapter: vercel({
     webAnalytics: {
-      enabled: true,
-    },
-  }),
+      enabled: true
+    }
+  })
 });
